aa25d57381b5e2b91ce1bbee7bd228b5bec91eae
daly <daly@172.16.211.228>
2015-09-28 10:50:18 +0200

    conf jpa project
    Signed-off-by: daly <daly@172.16.211.228>

---
+++ key-hr/src/main/java/entities/Employee.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_EMP")
+public class Employee implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+
+	public Employee() {
+	}
+
+	public Employee(String name) {
+		this.name = name;
+	}
+
+	@Id
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
